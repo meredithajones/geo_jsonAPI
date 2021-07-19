34df268aaeee5977be83f07f8bb04cf11dc0aa6f
Meredith Jones <mj19003@gmail.com>
2021-07-19 16:01:46 -0400

    Enabled CORS, set up middleware and wrote sample route for testing

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ dotenv.config({ path: './config/config.env'});
 
 const app = express();
 
+//Body Parser Middleware
+app.use(express.json());
+
+//Enable CORS
+app.use(cors());
+
+// sample route
+app.get('/api/v1/stores', (req, res) => {
+    res.send('hello');
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV}
