b6414dfe4f0ac1dfc2fd5e4474f350e0f0d59b02
Meredith Jones <mj19003@gmail.com>
2021-07-20 14:07:54 -0400

    Added location info to Store Schema

--- models/Store.js
+++ models/Store.js
@@ -7,5 +7,23 @@ const StoreSchema = new mongoose.Schema({
         unique: true,
         trim: true,
         maxlength: [10, 'Store ID must be less than 10 chars']
-    }
-})
+    },
+    //Boilerplate from mongoose docs
+    location: {
+        type: {
+          type: String, 
+          enum: ['Point'], // 'location.type' must be 'Point'
+        },
+        coordinates: {
+          type: [Number],
+          index: '2dsphere'
+        },
+        formattedAddress: String
+      },
+      createdAt: {
+          type: Date,
+          default: Date.now
+      }
+});
+
+module.exports = mongoose.model('Store', StoreSchema);
