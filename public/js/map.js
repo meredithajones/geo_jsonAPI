aba51d39c6d7e721e9a6bcdf0c069de29faa1d88
Meredith Jones <mj19003@gmail.com>
2021-07-21 15:07:23 -0400

    Addeed mapbox token and map in front end js file

---
+++ public/js/map.js
@@ -0,0 +1,8 @@
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWVyam80NyIsImEiOiJja3JkdTE1eW4xdm9xMnVscXJjNm5vOGN2In0.PQy4zd0oTnMTcRPtWwgTKw';
+const map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+zoom: 9, 
+center: [-71.157895, 42.707741]
+});
