dc914fcacf108199d42eef276564f9889efdd44b
Meredith Jones <mj19003@gmail.com>
2021-07-22 16:33:38 -0400

    Added to layout object in loadMap function

--- public/js/map.js
+++ public/js/map.js
@@ -1,26 +1,32 @@
-
-mapboxgl.accessToken = 'pk.eyJ1IjoibWVyam80NyIsImEiOiJja3JkdTE1eW4xdm9xMnVscXJjNm5vOGN2In0.PQy4zd0oTnMTcRPtWwgTKw';
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibWVyam80NyIsImEiOiJja3JkdTE1eW4xdm9xMnVscXJjNm5vOGN2In0.PQy4zd0oTnMTcRPtWwgTKw";
 const map = new mapboxgl.Map({
-container: 'map',
-style: 'mapbox://styles/mapbox/streets-v11',
-zoom: 9, 
-center: [-75.165222, 39.952583]
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  zoom: 9,
+  center: [-75.165222, 39.952583],
 });
 
-function loadMap(){
+function loadMap() {
     map.on('load', function() {
-// Add a layer to use the image to represent the data.
-map.addLayer({
-    'id': 'points',
-    'type': 'symbol',
-    'source': 'point', // reference the data source
-    'layout': {
-    'icon-image': 'cat', // reference the image
-    'icon-size': 0.25
-}
-});
-});
-}
-
-     
-    
+        map.addLayer({
+          id: 'points',
+          type: 'symbol',
+          source: {
+            type: 'geojson',
+            data: {
+              type: 'FeatureCollection',
+              features: stores
+            }
+          },
+          layout: {
+            'icon-image': '{icon}-15',
+            'icon-size': 1.5,
+            'text-field': '{storeId}',
+            'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+            'text-offset': [0, 0.9],
+            'text-anchor': 'top'
+          }
+        });
+      });
+    }
