4d522fecccc50fc585da5849688d08a0d7ee288d
Meredith Jones <mj19003@gmail.com>
2021-07-22 16:28:03 -0400

    Added loadMap function to map.js

--- public/js/map.js
+++ public/js/map.js
@@ -4,5 +4,23 @@ const map = new mapboxgl.Map({
 container: 'map',
 style: 'mapbox://styles/mapbox/streets-v11',
 zoom: 9, 
-center: [-71.157895, 42.707741]
+center: [-75.165222, 39.952583]
 });
+
+function loadMap(){
+    map.on('load', function() {
+// Add a layer to use the image to represent the data.
+map.addLayer({
+    'id': 'points',
+    'type': 'symbol',
+    'source': 'point', // reference the data source
+    'layout': {
+    'icon-image': 'cat', // reference the image
+    'icon-size': 0.25
+}
+});
+});
+}
+
+     
+    
