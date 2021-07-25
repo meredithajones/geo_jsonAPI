8e0523ecd0551ebc5b00ac7298c7f506bc7567b1
Meredith Jones <mj19003@gmail.com>
2021-07-25 14:00:59 -0400

    Redoing map.js to work out bugs -not hitting API from front end

--- public/js/map.js
+++ public/js/map.js
@@ -1,60 +1,50 @@
 mapboxgl.accessToken =
   "pk.eyJ1IjoibWVyam80NyIsImEiOiJja3JkdTE1eW4xdm9xMnVscXJjNm5vOGN2In0.PQy4zd0oTnMTcRPtWwgTKw";
-  const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    zoom: 9,
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  zoom: 10,
   center: [-75.165222, 39.952583],
 });
 
-// Fetch stores from API
-async function getStores() {
-  const res = await fetch('/api/v1/stores');
-  const data = await res.json();
-
-  const stores = data.data.map(store => {
-    return {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [
-          store.location.coordinates[0],
-          store.location.coordinates[1]
-        ]
-      },
-      properties: {
-        storeId: store.storeId,
-        icon: 'shop'
-      }
-    };
+map.on('load', function () {
+  // Load an image from an external URL.
+  map.loadImage(
+  'https://docs.mapbox.com/mapbox-gl-js/assets/cat.png',
+  function (error, image) {
+  if (error) throw error;
+   
+  // Add the image to the map style.
+  map.addImage('cat', image);
+   
+  // Add a data source containing one point feature.
+  map.addSource('point', {
+  'type': 'geojson',
+  'data': {
+  'type': 'FeatureCollection',
+  'features': [
+  {
+  'type': 'Feature',
+  'geometry': {
+  'type': 'Point',
+  'coordinates': [-77.4144, 25.0759]
+  }
+  }
+  ]
+  }
   });
-
-  loadMap(stores);
-}
-
-// Load map with stores
-function loadMap(stores) {
-  map.on('load', function() {
-    map.addLayer({
-      id: 'points',
-      type: 'symbol',
-      source: {
-        type: 'geojson',
-        data: {
-          type: 'FeatureCollection',
-          features: stores
-        }
-      },
-      layout: {
-        'icon-image': '{icon}-15',
-        'icon-size': 1.5,
-        'text-field': '{storeId}',
-        'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
-        'text-offset': [0, 0.9],
-        'text-anchor': 'top'
-      }
-    });
+   
+  // Add a layer to use the image to represent the data.
+  map.addLayer({
+  'id': 'points',
+  'type': 'symbol',
+  'source': 'point', // reference the data source
+  'layout': {
+  'icon-image': 'cat', // reference the image
+  'icon-size': 0.25
+  }
   });
-}
-
-getStores();
+  }
+  );
+  });
+  
