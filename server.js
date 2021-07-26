3aa18d7afde1a073e86e61d1a3aa02539566aaee
Meredith Jones <mj19003@gmail.com>
2021-07-26 16:25:39 -0400

    Redoing database connection

--- server.js
+++ server.js
@@ -19,15 +19,13 @@ app.use(express.json());
 app.use(cors());
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.use('/api/v1/stores', require('./routes/stores'));
+// app.use('/api/v1/stores', require('./routes/stores'));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
-
-console.log(__dirname);
