e21d9430c747023211bddd2a66b21f90de1765ae
Meredith Jones <mj19003@gmail.com>
2021-08-06 16:03:23 -0400

    Updating loadMap function

--- public/js/map2.js
+++ public/js/map2.js
@@ -8,30 +8,20 @@ const map = new mapboxgl.Map({
 });
 
 function loadMap () {
-    map.addSource('point', {
-        'type': 'geojson',
-        'data': {
-        'type': 'FeatureCollection',
-        'features': [
-        {
-        'type': 'Feature',
-        'geometry': {
-        'type': 'Point',
-        'coordinates': [-77.4144, 25.0759]
-        }
-        }
-        ]
-        }
-        });
-         
         // Add a layer to use the image to represent the data.
         map.addLayer({
-        'id': 'points',
-        'type': 'symbol',
-        'source': 'point', // reference the data source
-        'layout': {
-        'icon-image': 'cat', // reference the image
-        'icon-size': 0.25
+        id: 'points',
+        type: 'symbol',
+        source: {
+             type: 'geojson',
+        data: {
+            type: 'FeatureCollection',
+            'features': [
+            {
+            'type': 'Feature',
+            'geometry': {
+            'type': 'Point',
+            'coordinates': [-77.4144, 25.0759]
         }
         });
         
