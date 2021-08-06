c8094b4182180d70ec7ff30c26729f4375024673
Meredith Jones <mj19003@gmail.com>
2021-08-06 15:51:51 -0400

    Added loadmap function

--- public/js/map2.js
+++ public/js/map2.js
@@ -6,3 +6,33 @@ const map = new mapboxgl.Map({
   zoom: 12,
   center: [-75.165222, 39.952583],
 });
+
+function loadMap () {
+    map.addSource('point', {
+        'type': 'geojson',
+        'data': {
+        'type': 'FeatureCollection',
+        'features': [
+        {
+        'type': 'Feature',
+        'geometry': {
+        'type': 'Point',
+        'coordinates': [-77.4144, 25.0759]
+        }
+        }
+        ]
+        }
+        });
+         
+        // Add a layer to use the image to represent the data.
+        map.addLayer({
+        'id': 'points',
+        'type': 'symbol',
+        'source': 'point', // reference the data source
+        'layout': {
+        'icon-image': 'cat', // reference the image
+        'icon-size': 0.25
+        }
+        });
+        
+}
